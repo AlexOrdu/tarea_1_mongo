a2a71fe3e0f99b753c76ee447d45802705b5eb95
AlexOrdu <alexmaxol06@gmail.com>
2017-03-16 15:11:43 -0700

    Tarea 1 de mongo, archivo con extención .js que contiene los comandos realizados

---
+++ tarea_1.js
@@ -0,0 +1,63 @@
+//***************************************************************
+//1) Baja el archivo grades.json y en la terminal ejecuta el siguiente comando:
+$ mongoimport -d students -c grades < grades.json
+
+//***************************************************************
+//2) El conjunto de datos contiene 4 calificaciones de n estudiantes. Confirma que se importo correctamente la colección con los siguientes comandos en la terminal de mongo:
+use students;
+db.grades.count();
+
+//cuantos registros arrojo el comando count?
+//800 registros
+
+//***************************************************************
+//3) Encuentra todas las calificaciones del estudiante con el id numero 4
+db.grades.find({student_id:4});
+/*{ "_id" : ObjectId("50906d7fa3c412bb040eb587"), "student_id" : 4, "type" : "exam", "score" : 87.89071881934647 }
+{ "_id" : ObjectId("50906d7fa3c412bb040eb588"), "student_id" : 4, "type" : "quiz", "score" : 27.29006335059361 }
+{ "_id" : ObjectId("50906d7fa3c412bb040eb589"), "student_id" : 4, "type" : "homework", "score" : 5.244452510818443 }
+{ "_id" : ObjectId("50906d7fa3c412bb040eb58a"), "student_id" : 4, "type" : "homework", "score" : 28.656451042441 }*/
+
+
+//***************************************************************
+//4) ¿Cuantos registros hay de tipo exam?
+db.grades.count({"type":"exam"});
+/*200 registros de tipo exam*/
+
+
+//***************************************************************
+//5) ¿Cuantos registros hay de tipo homework?
+db.grades.count({"type":"homework"});
+/*400 registros de tipo homework*/
+
+
+//***************************************************************
+//6) ¿Cuantos registros hay de tipo quiz?
+db.grades.count({"type":"quiz"});
+/*200 registros de tipo quiz*/
+
+
+//***************************************************************
+//7) Elimina todas las calificaciones del estudiante con el id numero 3
+db.grades.remove({student_id: 3});
+/*WriteResult({ "nRemoved" : 4 })*/
+
+//***************************************************************
+//8) ¿Que estudiantes obtuvieron 75.29561445722392 en una tarea ?
+db.grades.count({"type" : "homework", "score":75.29561445722392});
+/*1*/
+db.grades.find({"type":"exam"});
+/*{ "_id" : ObjectId("50906d7fa3c412bb040eb59e"), "student_id" : 9, "type" : "homework", "score" : 75.29561445722392 }*/
+
+//***************************************************************
+//9) Actualiza las calificaciones del registro con el uuid 50906d7fa3c412bb040eb591 por 100
+db.grades.save({ "_id" : ObjectId("50906d7fa3c412bb040eb591"), "student_id" : 6, "type" : "homework", "score" : 100 });
+/*WriteResult({ "nMatched" : 1, "nUpserted" : 0, "nModified" : 0 })*/
+
+//***************************************************************
+//10) A que estudiante pertenece esta calificación.
+db.grades.find({ "_id" : ObjectId("50906d7fa3c412bb040eb591")});
+/*/*{ "_id" : ObjectId("50906d7fa3c412bb040eb591"), "student_id" : 6, "type" : "homework", "score" : 100 }*/
+db.grades.find({"score":100});
+/*{ "_id" : ObjectId("50906d7fa3c412bb040eb591"), "student_id" : 6, "type" : "homework", "score" : 100 }
+Es el estudiante con el student_id:6*/
